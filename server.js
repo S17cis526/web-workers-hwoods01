2b3714b2d53c2f8a847f37c247cb647f97d35074
hayden <hayden@hayzak.com>
2017-04-25 14:17:43 -0500

    Class completions

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ function handleRequest(req, res) {
     case '/script.js':
       serveFile('public/script.js', 'text/css', req, res);
       break;
+    // serve worker sripts
+    case '/permutations.js':
+      serveFile('public/permutations.js', 'text/javascript', req, res);
+      break;
+    case '/grayscale.js':
+      serveFile('public/grayscale.js', 'text/javascript', req, res);
+      break;
     // Serving image files
     case '/images/ace.jpg':
       serveFile('images/ace.jpg', 'image/jpeg', req, res);
